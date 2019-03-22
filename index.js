81cf6f3b4be2779ceb7a08870751935da499bf20
Chris Hartgerink <chris@libscie.org>
2019-03-22 12:11:57 +0100

    Add skeleton functions

--- index.js
+++ index.js
@@ -2,15 +2,70 @@ const Dat = require('dat-node')
 const fs = require('fs')
 const path = require('path')
 
-// get environment variable
 let LIBSCIE_DIR = process.env.LIBSCIE
 // create the environment directory if not available
 
+function init (type) {
+    // create skeleton metadata file
+    let datJSON = initSkel(type)
+    datJSON.title = title
+    datJSON.description = desc
+    // put metadata in folder
+    // create tmp folder if not existent
+    // init dat in folder (don't share yet)
+    Dat(path.join(LIBSCIE_DIR, 'tmp'), (err, dat) => {
+        if (err) throw err
 
-// create profile
-	// create Dat (without sharing)
-	// create scholarly profile
-	// move to environment
+        dat.importFiles()
+        dat.key.toString('hex')
+        // rename folder to hash
+        fs.rename(path.join(LIBSCIE_DIR, 'tmp'),
+                  path.join(LIBSCIE_DIR, hash), (err) => {
+                      if (err) throw err
+                  })
+    })
+}
+
+// init skeleton for dat.json
+// no more than that
+function initSkel (type) {
+    let jsonSkel = {}
+
+    jsonSkel.title = ''
+    jsonSkel.description = ''
+    jsonSkel.url = '' // need to preprocess this
+    jsonSkel.parents = []
+    jsonSkel.roots = []
+    jsonSkel.main = ''
+
+    if (type === 'profile') {
+        jsonSkel = initProfile(jsonSkel)
+    } else if (type === 'module') {
+        jsonSkel = initModule(jsonSkel)
+    } else {
+        throw "Something went wrong. Error code 001."
+    }
+
+    return jsonSkel
+}
+
+// add profile skeleton
+function initProfile (obj) {
+    obj.type = "scholarly-profile"
+    obj.follows= []
+    obj.modules= []
+
+    return obj
+}
+
+
+// add module skeleton
+function initModule (obj) {
+    obj.type= "scholarly-module",
+    obj.authors= []
+
+    return obj
+}
 
 // create module
 
@@ -20,12 +75,9 @@ let LIBSCIE_DIR = process.env.LIBSCIE
 // save a checked out version of the module
 // put the versioned link in the profile
 
-const Dat = require('dat-node')
-const path = require('path')
-
 // clone module (clean)
 function clone (hash) {
-    Dat(path.join(process.env.LIBSCIE_DIR, hash), {
+    Dat(path.join(LIBSCIE_DIR, hash), {
         key: hash
     }, (err, dat) => {
         if (err) throw err
@@ -50,3 +102,11 @@ function clone (hash) {
 // visualize network
 
 // export keys
+
+// clean cache
+
+
+// testing
+
+console.log(initSkel('profile'))
+console.log(initSkel('module'))
