a6a2455d081800f4e0c96438b0e8019a1ac5eadf
Chris Hartgerink <chris@libscie.org>
2019-03-29 21:44:14 +0100

    Initialize functions
    This commit initializes hashChecker, hashShort, checkout, and clone.

--- index.js
+++ index.js
@@ -8,25 +8,96 @@ const register = require('./lib/register')
 let LIBSCIE_DIR = process.env.LIBSCIE_DIR
 console.log(LIBSCIE_DIR)
 
-// share profile/module
+// set options
+// function sets the defaults, arguments can be adjusted
+// to deviate from those defaults
 
-// clone module (clean)
-function clone (hash) {
-    Dat(path.join(LIBSCIE_DIR, hash), {
-        key: hash
-    }, (err, dat) => {
+function hashShort (x) {
+    let p = x.substr(0, 3)
+    let q = x.substr(x.length - 3, x.length)
+    return `${p}...${q}`
+}
+
+function hashChecker (x) {
+    let res = {}
+    x = x.split('+')
+    // check for dat://
+    // doesn't crash if not present
+    res.key = x[0].replace('dat://', '')
+    res.version = parseInt(x[1])
+
+    return res
+}
+// hashChecker('dat://123+2')
+// hashChecker('123+2')
+// hashChecker('dat://123')
+// hashChecker('123')
+
+function clone (x) {
+    let info = hashChecker(x)
+    // check whether dat already cached! TODO
+    Dat(path.join(LIBSCIE_DIR, info.key),
+        { key: info.key,
+          sparse: false,
+          temp: false
+        }, (err, dat) => {
+            if (err) throw err
+            
+            dat.joinNetwork((err, res) => {
+                if (err) throw err
+
+                dat.leaveNetwork()
+                console.log(`Finished replicating dat://${hashShort(info.key)}`)
+            })
+        })
+}
+
+function checkout (x) {
+    let info = hashChecker(x)
+
+    Dat(path.join(LIBSCIE_DIR, info.key), (err, dat) => {
         if (err) throw err
-
-        dat.joinNetwork((err, res) => {
+        
+        let oldDat = dat.archive.checkout(info.version)
+        
+        oldDat.readdir('./', {cached:true}, (err, res) => {
             if (err) throw err
 
-            dat.leaveNetwork()
-            console.log('Finished replicating dat://' + hash)
+            console.log(res)
         })
+        
+        return oldDat
     })
 }
 
+
 // verify module
+function verify (module) {
+    let info = hashChecker(module)
+    if ( !info.version ) {
+        throw 'Error: verification requires versioned module'
+    }
+
+    Dat(path.join(LIBSCIE_DIR, info.key),
+        { key: info.key,
+          sparse: false,
+          temp: false
+        }, (err, dat) => {
+            if (err) throw err
+
+            dat.joinNetwork((err, res) => {
+                if (err) throw err
+
+                res.archive.checkout(info.version)
+            })
+        })
+    // REQ versioned module?
+    // get metadata
+    // REQ 'scholarly-module'
+    // get authors
+    // for i in authors check whether mentions module
+    // iff true for all authors, then true
+}
 
 // trawl N degrees of separation from profile
 
@@ -38,7 +109,9 @@ function clone (hash) {
 
 // export keys
 
-// clean cache
+// cache operations
+// check size
+// clean (how?)
 
 
 // testing
@@ -46,9 +119,12 @@ function clone (hash) {
 init('profile', 'CHJ Hartgerink', LIBSCIE_DIR)
 init('module', 'Dear Diary', LIBSCIE_DIR)
 
-register('c28c17ed0a8580f09b29d608b165b6d135b9bdde9decd9fcf8aa8423277ca585',
-         '52782b10068b0d48c9c04b9b8c693fffff4a3874a0d181f4f074d9095aa891dd',
-         LIBSCIE_DIR)
+// register('c28c17ed0a8580f09b29d608b165b6d135b9bdde9decd9fcf8aa8423277ca585',
+//          '52782b10068b0d48c9c04b9b8c693fffff4a3874a0d181f4f074d9095aa891dd',
+//          LIBSCIE_DIR)
+
+clone('dat://50025da67776ab531163bf2bba5849db5e8c396dc5144a34db6e85d48f626609+2')
+clone('dat://50025da67776ab531163bf2bba5849db5e8c396dc5144a34db6e85d48f626609+4')
 
 // register('',
 //          '',
