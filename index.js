f2bf08822170abaf2aa5f6c92fb079e2533e78d1
Diego Paez <paez.di@gmail.com>
2019-10-10 16:46:57 -0300

    Update: WIP adding local db support using level. Also add serialization support with avro

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const { homedir, tmpdir, platform } = require('os')
 const {
   promises: { writeFile, rename }
 } = require('fs')
-const fs = require('fs-extra')
-const RAM = require('random-access-memory')
+const { ensureDir } = require('fs-extra')
+const level = require('level')
+const avro = require('avsc')
+const envPaths = require('env-paths')
 const discovery = require('hyperdiscovery')
 const uniqueString = require('unique-string')
 const DatEncoding = require('dat-encoding')
-const debug = require('debug')('libsdk')
+const debug = require('debug')('p2pcommons')
 const DatHelper = require('./lib/dat-helper')
+const ContentSchema = require('./schemas/content.json')
+const ProfileSchema = require('./schemas/profile.json')
 
 const DEFAULT_SWARM_OPTS = {
   extensions: []
@@ -21,7 +25,6 @@ const createDatJSON = (type, title = '', description = '') => {
   obj.title = title
   obj.description = description
   obj.url = ''
-  obj.roots = []
   obj.main = ''
   obj.license = ''
 
@@ -40,8 +43,10 @@ const createDatJSON = (type, title = '', description = '') => {
 
 class SDK {
   constructor (opts = {}) {
-    debug('libsdk:constructor')
+    debug('p2pcommons:constructor')
+    this.paths = envPaths('p2pcommons')
     this.platform = platform()
+    // NOTE(dk): consider switch to envPaths usage
     this.home =
       process.env.HOME ||
       (this.windows && this.windowsHome()) ||
@@ -52,13 +57,48 @@ class SDK {
     this.baseDir = join(this.home, opts.baseDir)
     this.persist = opts.persist || true
     this.storage = opts.storage || undefined
-    // instantiate local db here
+    this.dbPath = opts.dbPath || this.paths.data
     // start hyperswarm
     this.disableSwarm = !!opts.disableSwarm
     if (!this.disableSwarm) {
-      debug('libsdk:constructor starting swarm')
+      debug('p2pcommons:constructor starting swarm')
       this.swarm = discovery(Object.assign({}, DEFAULT_SWARM_OPTS, opts.swarm))
     }
+  }
+
+  async ready () {
+    await ensureDir(this.dbPath)
+    return new Promise((resolve, reject) => {
+      // start local db
+      this.contentType = avro.Type.forSchema(ContentSchema)
+      this.profileType = avro.Type.forSchema(ProfileSchema)
+      this.contentTypeCodec = {
+        type: 'AvroContentType',
+        encode: datum => this.contentType.toBuffer(datum),
+        decode: datum => this.contentType.fromBuffer(datum),
+        buffer: true
+      }
+      this.profileTypeCodec = {
+        type: 'AvroProfileType',
+        encode: datum => this.profileType.toBuffer(datum),
+        decode: datum => this.profileType.fromBuffer(datum),
+        buffer: true
+      }
+      level(
+        join(this.dbPath, 'db'),
+        {
+          valueEncoding: 'binary'
+        },
+        (err, db) => {
+          if (err instanceof level.errors.OpenError) {
+            console.error('p2pcommons:failed to open database')
+            reject(err)
+          }
+          this.db = db
+          resolve()
+        }
+      )
+    })
   }
 
   async init ({ type, title = '', description = '', ...rest }) {
@@ -72,12 +112,12 @@ class SDK {
       type.endsWith('profile') || type.endsWith('content'),
       "type should be 'content' or 'profile'"
     )
-    debug(`libsdk:init ${type}`)
+    debug(`p2pcommons:init ${type}`)
     const datJSON = createDatJSON(type, title, description)
 
     const tmp = join(this.baseDir, uniqueString())
 
-    await fs.ensureDir(tmp)
+    await ensureDir(tmp)
 
     const datHelper = await DatHelper(tmp, {
       persist: this.persist,
@@ -89,12 +129,12 @@ class SDK {
 
     const hash = archive.key.toString('hex')
 
-    debug('libsdk:hash', hash)
+    debug('p2pcommons:hash', hash)
     if (!this.disableSwarm) {
       this.swarm.add(archive)
 
       archive.once('close', () => {
-        debug(`libsdk:closing archive ${archive.publicKey}...`)
+        debug(`p2pcommons:closing archive ${archive.publicKey}...`)
         const discoveryKey = DatEncoding.encode(archive.discoveryKey)
         this.swarm.leave(discoveryKey)
         this.swarm._replicatingFeeds.delete(discoveryKey)
@@ -111,14 +151,17 @@ class SDK {
     await rename(tmp, join(this.baseDir, hash))
 
     console.log(`Initialized new ${datJSON.type}, dat://${hash}`)
-
+    debug('p2pcommons:datJSON', datJSON)
     // cache(hash, env)
-
+    const codec =
+      datJSON.type === 'content' ? this.contentTypeCodec : this.profileTypeCodec
+    await this.db.put(hash, datJSON, { valueEncoding: codec })
+    console.log(`Saved new ${datJSON.type}, wth key: ${hash}`)
     return datJSON
   }
 
   async destroy () {
-    debug('libsdk:destroying swarm')
+    debug('p2pcommons:destroying swarm')
     if (this.disableSwarm) return
     return this.swarm.close()
   }
