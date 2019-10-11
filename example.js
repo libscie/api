4c56644208cef45960102a56fbd590d0fc282a87
Diego Paez <paez.di@gmail.com>
2019-10-11 17:14:00 -0300

    Update: transition example to sublevel

--- example.js
+++ example.js
@@ -6,13 +6,13 @@ process.once('SIGINT', () => commons.destroy())
   await commons.ready() // initializes local db
   const contentMetadata = await commons.init({ type: 'content' }) // ~/.p2pcommons/hash/dat.json --> type: content
   await commons.init({ type: 'profile' }) // ~/.p2pcommons/hash/dat.json --> type: profile
-  const contentHash = contentMetadata.url.toString('hex')
-  const out = await commons.get(contentHash)
+  const key = `content_${contentMetadata.url.toString('hex')}`
+  const out = await commons.get(key)
   console.log(`Retrieved type: ${out.type}`)
   out.title = 'Sample Content!'
   console.log('Updating content...')
-  await commons.set(contentHash, out)
+  await commons.set(key, out)
   // check out updated value from local db
-  const result = await commons.get(contentHash)
+  const result = await commons.get(key)
   console.log('Content updated', result)
 })()
