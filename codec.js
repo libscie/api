73ca76b5e697c088e6fd7067844d58c712857c03
Diego Paez <paez.di@gmail.com>
2019-10-11 17:16:02 -0300

    Add: new simple codec for level

---
+++ codec.js
@@ -0,0 +1,53 @@
+const assert = require('assert')
+const debug = require('debug')('p2pcommons')
+const { Type } = require('@avro/types')
+const GenericType = require('./schemas/generic.json')
+
+class Codec {
+  constructor (registry) {
+    assert.ok(registry, 'registry is required')
+    Type.forSchema(GenericType, { registry })
+    this.registry = registry
+    debug('p2pcommons:Codec:registry', this.registry)
+    this.buffer = true
+    this.type = 'AvroTypes'
+  }
+
+  encode (obj) {
+    debug('p2pcommons:Codec:encode', obj)
+    assert.ok(obj, 'object is required')
+    assert.strictEqual(
+      typeof obj,
+      'object',
+      `Expected an object. Received: ${obj}`
+    )
+    assert.strictEqual(
+      typeof obj.type,
+      'string',
+      'object should have a type property'
+    )
+    assert.ok(obj.value, 'obj should have a value property')
+    const newVal = this.registry[obj.type].toBuffer(obj.value)
+    // return a new generic
+    return this.registry.Generic.toBuffer({ type: obj.type, value: newVal })
+  }
+
+  decode (buf) {
+    assert.ok(buf, 'buffer is required')
+    const { type, value } = this.decodeGeneric(buf)
+    return this.registry[type].fromBuffer(value)
+  }
+
+  decodeGeneric (buf) {
+    try {
+      const obj = this.registry.Generic.fromBuffer(buf)
+      return obj
+    } catch (err) {
+      // Note(dk): improve error handling here - unknown type...
+      console.error(err)
+      return buf
+    }
+  }
+}
+
+module.exports = Codec
