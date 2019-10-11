d185a81d6bef89792ddd66bdeb1469f643d4ac1f
Diego Paez <paez.di@gmail.com>
2019-10-11 17:11:01 -0300

    Update: improve example with basic content fetch and update

--- example.js
+++ example.js
@@ -4,6 +4,15 @@ const commons = P2PCommons({ disableSwarm: true })
 process.once('SIGINT', () => commons.destroy())
 ;(async () => {
   await commons.ready() // initializes local db
-  await commons.init({ type: 'content' }) // ~/.p2pcommons/hash/dat.json --> type: content
+  const contentMetadata = await commons.init({ type: 'content' }) // ~/.p2pcommons/hash/dat.json --> type: content
   await commons.init({ type: 'profile' }) // ~/.p2pcommons/hash/dat.json --> type: profile
+  const contentHash = contentMetadata.url.toString('hex')
+  const out = await commons.get(contentHash)
+  console.log(`Retrieved type: ${out.type}`)
+  out.title = 'Sample Content!'
+  console.log('Updating content...')
+  await commons.set(contentHash, out)
+  // check out updated value from local db
+  const result = await commons.get(contentHash)
+  console.log('Content updated', result)
 })()
