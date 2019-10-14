1bfc22a7beb26e5fbf4cacd45ac2db48376ec077
Diego Paez <paez.di@gmail.com>
2019-10-14 11:23:32 -0300

    Update: add more content + get/set + search

--- example.js
+++ example.js
@@ -3,16 +3,44 @@ const commons = P2PCommons({ disableSwarm: true })
 
 process.once('SIGINT', () => commons.destroy())
 ;(async () => {
-  await commons.ready() // initializes local db
-  const contentMetadata = await commons.init({ type: 'content' }) // ~/.p2pcommons/hash/dat.json --> type: content
+  // initializes local db first
+  await commons.ready()
+
+  // create some content
+  const contentMetadata1 = await commons.init({ type: 'content' }) // ~/.p2pcommons/hash/dat.json --> type: content
+  const contentMetadata2 = await commons.init({
+    type: 'content',
+    title: 'Sample Content 2',
+    description: 'lorem ipsum alfa beta'
+  }) // ~/.p2pcommons/hash/dat.json --> type: content
+  const contentMetadata3 = await commons.init({
+    type: 'content',
+    title: 'Reference content',
+    description: 'lorem ipsum'
+  }) // ~/.p2pcommons/hash/dat.json --> type: content
+
+  // create a profile
   await commons.init({ type: 'profile' }) // ~/.p2pcommons/hash/dat.json --> type: profile
-  const key = `content_${contentMetadata.url.toString('hex')}`
-  const out = await commons.get(key)
+  const key = contentMetadata1.url.toString('hex')
+  const out = await commons.get('content', key)
   console.log(`Retrieved type: ${out.type}`)
-  out.title = 'Sample Content!'
+  out.title = 'Sample Content'
+  out.description = 'This is a short abstract about nothing'
   console.log('Updating content...')
-  await commons.set(key, out)
+  await commons.set(out)
+
   // check out updated value from local db
-  const result = await commons.get(key)
+  const result = await commons.get('content', key)
   console.log('Content updated', result)
+
+  // filter content
+  const criteria = 'abstract about nothing'
+  const feature = 'description'
+  const filter = await commons.filter(feature, criteria)
+  console.log(`Results with ${feature}: ${criteria}`, filter.length)
+
+  const allContent = await commons.allContent()
+  const allProfiles = await commons.allProfiles()
+  console.log('Content length', allContent.length)
+  console.log('Profiles length', allProfiles.length)
 })()
