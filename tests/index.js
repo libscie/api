c5d7b438b0ae276a1b1a3c92461a3a825d607ce2
Diego Paez <paez.di@gmail.com>
2019-10-18 15:19:48 -0300

    Update: more tests

--- tests/index.js
+++ tests/index.js
@@ -53,3 +53,113 @@ test('init: create profile module', async t => {
   t.same(output.contents, [])
   t.end()
 })
+
+test('get: retrieve a value from the sdk', async t => {
+  const p2p = new SDK({
+    disableSwarm: true,
+    persist: false,
+    dbPath: tempy.directory()
+  })
+  await p2p.ready()
+  const sampleData = {
+    type: 'profile',
+    title: 'demo',
+    description: 'lorem ipsum'
+  }
+  const metadata = await p2p.init(sampleData)
+  const key = metadata.url.toString('hex')
+
+  const result = await p2p.get(key)
+
+  t.same(result, metadata)
+  t.end()
+})
+
+test('set: update a value', async t => {
+  const p2p = new SDK({
+    disableSwarm: true,
+    persist: false,
+    dbPath: tempy.directory()
+  })
+  await p2p.ready()
+  const sampleData = {
+    type: 'content',
+    title: 'demo',
+    description: 'lorem ipsum'
+  }
+  const metadata = await p2p.init(sampleData)
+  const key = metadata.url.toString('hex')
+
+  metadata.description = 'A more accurate description'
+  await p2p.set(metadata)
+  const result = await p2p.get(key)
+
+  t.same(result.description, metadata.description)
+  t.end()
+})
+
+test('list content', async t => {
+  const p2p = new SDK({
+    disableSwarm: true,
+    persist: false,
+    dbPath: tempy.directory()
+  })
+  await p2p.ready()
+  const sampleDataContent = [
+    {
+      type: 'content',
+      title: 'demo',
+      description: 'lorem ipsum'
+    },
+    {
+      type: 'content',
+      title: 'demo 2'
+    },
+    { type: 'content' }
+  ]
+
+  const sampleDataProfile = [{ type: 'profile' }]
+
+  await Promise.all(
+    []
+      .concat(sampleDataContent)
+      .concat(sampleDataProfile)
+      .map(d => p2p.init(d))
+  )
+  const result = await p2p.listContent()
+  t.same(result.length, sampleDataContent.length)
+  t.end()
+})
+
+test('list profiles', async t => {
+  const p2p = new SDK({
+    disableSwarm: true,
+    persist: false,
+    dbPath: tempy.directory()
+  })
+  await p2p.ready()
+  const sampleDataContent = [
+    {
+      type: 'content',
+      title: 'demo',
+      description: 'lorem ipsum'
+    },
+    {
+      type: 'content',
+      title: 'demo 2'
+    },
+    { type: 'content' }
+  ]
+
+  const sampleDataProfile = [{ type: 'profile' }]
+
+  await Promise.all(
+    []
+      .concat(sampleDataContent)
+      .concat(sampleDataProfile)
+      .map(d => p2p.init(d))
+  )
+  const result = await p2p.listProfiles()
+  t.same(result.length, sampleDataProfile.length)
+  t.end()
+})
